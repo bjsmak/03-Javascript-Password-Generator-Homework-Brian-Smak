31bafd558e6f1045b6f893ce9df6b6030a576071
Brian Smak <brian.j.smak@gmail.com>
2020-09-20 13:19:55 -0400

    defined variables, initial funcs

--- script.js
+++ script.js
@@ -12,3 +12,67 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Functions
+
+/*function numberRandom() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  console.log(numberRandom);
+}
+
+function lowerCaseRandom() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function upperCaseRandom() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function specialCharRandom() {
+  var specialChar = '!@#$%^&*<>~'
+  return specialChar[Math.floor(Math.random() * specialChar.length)]
+}****/
+
+ // Start variables
+
+ var promptLength = prompt("Please specify password length");
+ var upper = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
+ var lower = "abcdefghijklmnopqrstuvwxyz";
+ var number = "0123456789";
+ var specialChar = "!@#$%^&*()_-=+[]{}";
+ var initialPass = '';
+
+
+ //Conditionals
+
+ if (promptLength >= 8 && promptLength <= 128) {
+
+   var promptUpper = confirm("Would you like to include Uppercase Letters?");
+   var promptLower = confirm("Would you like to include Lowercase Letters?");
+   var promptNum = confirm("Would you like to include Numbers?");
+   var promptSpecChar = confirm("Would you like to include Special Characters?");
+
+   if (promptUpper === true) {
+      initialPass += upper;
+   }
+   if (promptLower === true) {
+      initialPass += lower;
+    }
+    if (promptNum === true) {
+      initialPass += number;    
+    }
+    if (promptSpecChar === true) {
+      initialPass += specialChar;    
+  }
+  
+  
+  if (promptUpper !== true && promptLower !== true && promptNum !== true && promptSpecChar !== true){ alert('You must choose at least one type of condition!');}
+
+ }
+
+ else {alert('Please specify between a number between 8 and 128 characters');}
+
+
+
