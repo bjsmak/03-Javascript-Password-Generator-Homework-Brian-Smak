0a98e363933a6863f1870df69b0c4edf04abcedc
Brian Smak <brian.j.smak@gmail.com>
2020-09-22 21:18:21 -0400

    overhauled functions, displays numbers properly

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+//Variables
+var specChar = "!@#$%^&*()<>";
+
+
+var generatePassword = function() {
+  //Set variables as containers for character types
+  var passwordZero = 0;
+  var upperZero = '';
+  var lowerZero = '';
+  var numZero = '';
+  var specZero = '';
+  var length = Number(prompt("Please specify password length"));
+
+  //Function array for random numbers
+  var funcArr = {
+    randomNum: function() {return String.fromCharCode(Math.floor(Math.random() * 10 + 48));},
+    randomLower: function() {return String.fromCharCode(Math.floor(Math.random() * 26 + 97));},
+    randomUpper: function() {return String.fromCharCode(Math.floor(Math.random() * 26 + 65));},
+    randomSpec: function() {return specChar[Math.floor(Math.random() * specChar.length)]}
+  }
+
+  if (length >= 8 && length <= 128) {
+  //console.log(length);
+
+  //If number selection is true then continue with prompts
+  var promptUpper = confirm("Would you like to include Uppercase Letters?");
+  var promptLower = confirm("Would you like to include Lowercase Letters?");
+  var promptNum = confirm("Would you like to include Numbers?");
+  var promptSpecChar = confirm("Would you like to include Special Characters?");
+
+    } else {alert('Please specify between a number between 8 and 128 characters');}
+
+  if (promptUpper !== true && promptLower !== true && promptNum !== true && promptSpecChar !== true) { alert('You must choose at least one type of condition!'); }
+  
+  //Concats arrays based on user selection. Adds random number and adds counter to password
+  if (promptLower === true) {
+    lowerZero = funcArr.randomLower();
+    passwordZero++;
+  }
+  
+  if (promptUpper === true) {
+    upperZero = funcArr.randomUpper();
+    passwordZero++;
+  }
+  
+  if (promptNum === true) {
+    numZero = funcArr.randomNum();
+    passwordZero++;
+  }
+  
+  if (promptSpecChar === true) {
+    specZero = funcArr.randomSpec();
+    passwordZero++;
+  }
+
+  
+  //Loop to generate random password with limit
+  var randomPass = '';
+  for (var i = 0; i < length-passwordZero; i++) {
+    randomPick = Math.floor(Math.random() * 4);
+    randomPass += randomPick;
+  }
+
+  return randomPass;
+}
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -5,80 +73,12 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-   //Conditionals for generating password
-  var length = Number(prompt("Please specify password length"));
-  if (length >= 8 && length <= 128) {
 
-    var promptUpper = confirm("Would you like to include Uppercase Letters?");
-    var promptLower = confirm("Would you like to include Lowercase Letters?");
-    var promptNum = confirm("Would you like to include Numbers?");
-    var promptSpecChar = confirm("Would you like to include Special Characters?");
- } else {alert('Please specify between a number between 8 and 128 characters');}
-
- if (promptUpper !== true && promptLower !== true && promptNum !== true && promptSpecChar !== true){ alert('You must choose at least one type of condition!');}
-  //console.log(length);  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Functions for Random Numbers
-
-function numberRandom() {
-  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function lowerCaseRandom() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function upperCaseRandom() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function specialCharRandom() {
-  var specialChar = '!@#$%^&*<>~'
-  return specialChar[Math.floor(Math.random() * specialChar.length)];
-}
-
-
- //Logging choices into generatePassword function
- var resultEl = document.getElementById('#password');
-
- document.getElementById('#generate').addEventListener('click', () => {
-
-
-
- })
-
-
- //Convert functions into Objects
-var randomAll = {
-  number: numberRandom,
-  lower: lowerCaseRandom,
-  upper: upperCaseRandom,
-  special: specialCharRandom
-};
-
-//Generate Password function
-function generatePassword(upper, lower, number, special, length) {
-  let initialPassword = '';
-  var typesCount = upper + lower + number + special;
-  var typesArr = [{upper}, {lower}, {number}, {special}].filter(item => Object.values(item)[0]);
-
-  //Loop to generate array
-  for(let i=0; i < length; i+=typesCount) {
-    typesArr.forEach(type => {
-        var typeLoop = Object.keys(type)[0];
-        initialPassword += randomAll[typeLoop]();
-    });
-  }
-
-  var finalPassword = initialPassword.slice(0, length);
-
-  return finalPassword;
-}
 
 
