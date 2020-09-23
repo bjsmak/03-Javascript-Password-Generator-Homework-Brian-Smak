cf6c012570e6e5338b308a375ecd6c1abcaca3f8
Brian Smak <brian.j.smak@gmail.com>
2020-09-23 14:51:01 -0400

    updated arrays, for loop properly adds in random characters, joins array

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-//Variables
-var specChar = "!@#$%^&*()<>";
-
-
+  
 var generatePassword = function() {
   //Set variables as containers for character types
-  var passwordZero = 0;
-  var upperZero = '';
-  var lowerZero = '';
-  var numZero = '';
-  var specZero = '';
   var length = Number(prompt("Please specify password length"));
+  //console.log(length);
 
   //Function array for random numbers
-  var funcArr = {
-    randomNum: function() {return String.fromCharCode(Math.floor(Math.random() * 10 + 48));},
-    randomLower: function() {return String.fromCharCode(Math.floor(Math.random() * 26 + 97));},
-    randomUpper: function() {return String.fromCharCode(Math.floor(Math.random() * 26 + 65));},
-    randomSpec: function() {return specChar[Math.floor(Math.random() * specChar.length)]}
-  }
+  //Variables
+    var num = ['1','2','3','4','5','6','7','8','9','0'];
+    var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var spec = ['!','@','#','$','%','^','&','*','(',')','<','>'];
 
   if (length >= 8 && length <= 128) {
-  //console.log(length);
 
   //If number selection is true then continue with prompts
   var promptUpper = confirm("Would you like to include Uppercase Letters?");
@@ -31,37 +22,39 @@ var generatePassword = function() {
     } else {alert('Please specify between a number between 8 and 128 characters');}
 
   if (promptUpper !== true && promptLower !== true && promptNum !== true && promptSpecChar !== true) { alert('You must choose at least one type of condition!'); }
-  
-  //Concats arrays based on user selection. Adds random number and adds counter to password
-  if (promptLower === true) {
-    lowerZero = funcArr.randomLower();
-    passwordZero++;
-  }
-  
-  if (promptUpper === true) {
-    upperZero = funcArr.randomUpper();
-    passwordZero++;
-  }
-  
-  if (promptNum === true) {
-    numZero = funcArr.randomNum();
-    passwordZero++;
-  }
-  
-  if (promptSpecChar === true) {
-    specZero = funcArr.randomSpec();
-    passwordZero++;
-  }
+    //Concats arrays based on user selection.
+      var randomPass =[];
+    if (promptLower === true) {
+      randomPass = randomPass.concat(lower);
+    }
+    if (promptUpper === true) {
+      randomPass = randomPass.concat(upper);
+    }
+    
+    if (promptNum === true) {
+      randomPass = randomPass.concat(num);
+    }
+    
+    if (promptSpecChar === true) {
+      randomPass = randomPass.concat(spec);
+    }
 
-  
-  //Loop to generate random password with limit
-  var randomPass = '';
-  for (var i = 0; i < length-passwordZero; i++) {
-    randomPick = Math.floor(Math.random() * 4);
-    randomPass += randomPick;
-  }
+        
 
-  return randomPass;
+  //console.log(randomPass);
+  console.log(length);
+  passwordFinal = [];
+  for (let i = 0; i < length; i++) {
+    //FOR LOOP TO ADD RANDOM NUMBER FROM ARRAY BASED ON IF/ELSE CONDITIONALS, PUSH TO ARRAY AND JOIN TO STRING
+    randomPass.join('');
+    randomNum = randomPass[Math.floor(Math.random() * (randomPass.length-1))];
+    console.log(randomNum);
+    passwordFinal.push(randomNum);
+    //console.log(passwordFinal);
+    
+  }
+  console.log(passwordFinal);
+  return passwordFinal.join("");
 }
 
 
